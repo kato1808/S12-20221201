37964a9250a71b2eb00d5f713f1a128455ea7d6b
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-12-01 11:20:16 +0900

    202212011120

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./styles.css";
+import theme from "./theme/theme";
 import { Button, ChakraProvider } from "@chakra-ui/react";
 
 export default function App() {
   return (
     <div className="App">
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Button colorScheme="teal">ボタン</Button>
+        <p>DDDDDDDDDDDDDDDD</p>
       </ChakraProvider>
     </div>
   );
