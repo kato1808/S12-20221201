982cd805017fa0a1f470c994e5386ca33ec684e1
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-12-01 11:44:26 +0900

    202212011144

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
 import theme from "./theme/theme";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <div className="App">
       <ChakraProvider theme={theme}>
-        <Button colorScheme="teal">ボタン</Button>
-        <p>DDDDDDDDDDDDDDDD</p>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </ChakraProvider>
     </div>
   );
