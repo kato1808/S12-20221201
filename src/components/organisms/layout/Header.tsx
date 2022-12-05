9270f214e5f0df97aca74d435d23c60619c63f6a
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-12-05 11:15:54 +0900

    202212051115

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,6 +1,5 @@
 import { memo, VFC } from "react";
 
 export const Header: VFC = memo(() => {
-  return;
-  <div style={{ height: "50px", backgroundColor: "teal" }}></div>;
+  return <div style={{ height: "50px", backgroundColor: "teal" }}></div>;
 });
