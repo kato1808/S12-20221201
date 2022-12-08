f2668a45b56ac38605a4a690d4989aaa301462ef
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-12-08 16:32:52 +0900

    202212081632

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,53 +1,22 @@
-import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
+import { Flex, Heading, Link } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Header: VFC = memo(() => {
   return (
-    <>
-      <Flex
-        as="nav"
-        bg="teal.500"
-        color="gray.50"
-        align="center"
-        justify="space-between"
-        padding={{ base: 3, md: 5 }}
-      >
-        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
-          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-            ユーザ管理アプリ
-          </Heading>
-        </Flex>
-        <Flex
-          align="center"
-          fontSize="sm"
-          flexGrow={2}
-          display={{ base: "none", md: "flex" }}
-        >
-          <Box pr={4}>
-            <Link>ユーザ一覧</Link>
-          </Box>
-          <Link>設定</Link>
-        </Flex>
-        {/* <IconButton
-          aria-label="メニューボタン"
-          icon={<HamburgerIcon />}
-          size="sm"
-          variant="unstyled"
-          display={{ base: "block", md: "none" }}
-          onClick={onOpen}
-        /> */}
+    <Flex
+      as="nav"
+      bg="teal.500"
+      color="gray.50"
+      align="center"
+      justify="space-between"
+      padding={{ base: 3, md: 5 }}
+    >
+      <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+        ユーザ管理アプリ
+      </Heading>
+      <Flex>
+        <Link>ユーザ一覧</Link>
       </Flex>
-      {/* <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerBody>
-              <Button>TOP</Button>
-              <Button>ユーザ一覧</Button>
-              <Button>設定</Button>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer> */}
-    </>
+    </Flex>
   );
 });
