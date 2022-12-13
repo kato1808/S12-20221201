bee6bff1070f4d2eae74fd1bc1fdcadf777ff050
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-12-13 15:40:42 +0900

    202212131540

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,35 +1,16 @@
-import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <Wrap>
+    <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          <Stack textAlign="center">
-            <Image
-              borderRadius="full"
-              boxSize="160px"
-              src="https://source.unsplash.com/random"
-              alt="プロフィール写真"
-              m="auto"
-            />
-            <Text fontSize="lg" fontWeight="bold">
-              加藤
-            </Text>
-            <Text fontSize="sm" color="gray">
-              HIROYUKI KATO
-            </Text>
-          </Stack>
-        </Box>
+        <UserCard
+          imageurl="https://source.unsplash.com/random"
+          userName="加藤"
+          fullName="加藤裕幸"
+        />
       </WrapItem>
     </Wrap>
   );
