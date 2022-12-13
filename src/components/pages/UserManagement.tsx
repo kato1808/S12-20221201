ee0dc3ef9d97e0f1bdb8113e457f94750a25f1a4
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-12-13 15:12:49 +0900

    202212131512

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,16 @@
+import { Box, Image, Stack, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザ管理ページです</p>;
+  return (
+    <Wrap>
+      <WrapItem>
+        <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md">
+          <Stack textAlign="center">
+            <Image src="https://source.unsplash.com/random" />
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
